bb469737171f770c6fad79d2e20c508cf16ab7cc
anddev404 <marcinek07@gmail.com>
2024-07-24 13:16:12 +0200

    Create HomeScreen.kt

---
+++ app/src/main/java/com/example/aimtrainer/HomeScreen.kt
@@ -0,0 +1,16 @@
+package com.example.aimtrainer
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.aimtrainer.navigation.Screen
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Button(onClick = {
+    }) {
+        Text(text = "Home Screen")
+    }
+
+}
