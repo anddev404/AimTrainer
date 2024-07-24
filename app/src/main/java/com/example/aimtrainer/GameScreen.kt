20877b672b89dbd50d792469a42d61254406bf3d
anddev404 <marcinek07@gmail.com>
2024-07-24 13:15:26 +0200

    Create GameScreen.kt

---
+++ app/src/main/java/com/example/aimtrainer/GameScreen.kt
@@ -0,0 +1,9 @@
+package com.example.aimtrainer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GameScreen() {
+    Text(text = "Game Screen")
+}
