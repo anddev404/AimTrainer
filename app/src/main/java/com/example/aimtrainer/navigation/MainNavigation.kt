de260c6549090245dce58683d8f60a9add9d177d
anddev404 <marcinek07@gmail.com>
2024-07-25 13:21:54 +0200

    Create MainNavigation.kt

---
+++ app/src/main/java/com/example/aimtrainer/navigation/MainNavigation.kt
@@ -0,0 +1,25 @@
+package com.example.aimtrainer.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.aimtrainer.GameScreen
+import com.example.aimtrainer.HomeScreen
+
+@Composable
+fun MainNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.MainScreen
+    ) {
+        composable<Screen.MainScreen> {
+            HomeScreen(navController = navController)
+        }
+        composable<Screen.GameScreen> {
+            GameScreen()
+        }
+    }
+}
