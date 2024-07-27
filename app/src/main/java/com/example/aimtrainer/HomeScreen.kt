1021b77a1c34b4a9f599011d9b63fa484b7637b9
anddev404 <marcinek07@gmail.com>
2024-07-27 13:53:57 +0200

    Add background to HomeScreen

--- app/src/main/java/com/example/aimtrainer/HomeScreen.kt
+++ app/src/main/java/com/example/aimtrainer/HomeScreen.kt
@@ -1,17 +1,22 @@
 package com.example.aimtrainer
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
-import com.example.aimtrainer.navigation.Screen
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Button(onClick = {
-        navController.navigate(Screen.GameScreen)
-    }) {
-        Text(text = "Home Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            painter = painterResource(id = R.drawable.background_main),
+            contentDescription = "",
+        )
     }
-
 }
