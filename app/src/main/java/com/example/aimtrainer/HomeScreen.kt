872fcfde4c999ad2d5d5f190866287e0d53f8917
anddev404 <marcinek07@gmail.com>
2024-07-27 14:04:09 +0200

    Add Play button to HomeScreen

--- app/src/main/java/com/example/aimtrainer/HomeScreen.kt
+++ app/src/main/java/com/example/aimtrainer/HomeScreen.kt
@@ -5,13 +5,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.aimtrainer.navigation.Screen
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -44,6 +52,24 @@ fun HomeScreen(navController: NavController) {
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "",
             )
+            Button(
+                onClick = { navController.navigate(Screen.GameScreen) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Unspecified),
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Image(
+                        modifier = Modifier.widthIn(200.dp),
+                        contentScale = ContentScale.FillWidth,
+                        painter = painterResource(id = R.drawable.button_play),
+                        contentDescription = "",
+                    )
+                    Text(
+                        text = "PLAY", fontSize = 36.sp,
+                    )
+                }
+            }
         }
     }
 }
