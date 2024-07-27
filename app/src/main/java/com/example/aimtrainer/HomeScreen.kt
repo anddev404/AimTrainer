0c26b472985fddb3745178c871ec955956e056f8
anddev404 <marcinek07@gmail.com>
2024-07-27 13:54:48 +0200

    Update HomeScreen.kt

--- app/src/main/java/com/example/aimtrainer/HomeScreen.kt
+++ app/src/main/java/com/example/aimtrainer/HomeScreen.kt
@@ -2,8 +2,10 @@ package com.example.aimtrainer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -16,6 +18,20 @@ fun HomeScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop,
             painter = painterResource(id = R.drawable.background_main),
+            contentDescription = "",
+        )
+        Image(
+            modifier = Modifier.fillMaxHeight(0.15f),
+            contentScale = ContentScale.FillHeight,
+            painter = painterResource(id = R.drawable.image_leaves_left),
+            contentDescription = "",
+        )
+        Image(
+            modifier = Modifier
+                .fillMaxHeight(0.15f)
+                .align(Alignment.TopEnd),
+            contentScale = ContentScale.FillHeight,
+            painter = painterResource(id = R.drawable.image_leaves_right),
             contentDescription = "",
         )
     }
