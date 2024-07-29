6c0edf2258704014b5df16a7d64b6b3588088571
anddev404 <marcinek07@gmail.com>
2024-07-29 18:02:05 +0200

    Add Divider to BottomTabView

--- app/src/main/java/com/example/aimtrainer/BottomTabView.kt
+++ app/src/main/java/com/example/aimtrainer/BottomTabView.kt
@@ -1,10 +1,27 @@
 package com.example.aimtrainer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true)
 @Composable
 fun BottomTabView() {
 
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Divider() {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth(),
+        painter = painterResource(id = R.drawable.divider),
+        contentDescription = "",
+        contentScale = ContentScale.FillBounds
+    )
 }
