1801b5d894118fb9b368c2cefb8d50f33299f74d
anddev404 <marcinek07@gmail.com>
2024-08-03 19:27:13 +0200

    Create GameViewModel.kt

---
+++ app/src/main/java/com/example/aimtrainer/game/presentation/GameViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.aimtrainer.game.presentation
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+    private var startTime = 0L
+    private var endTime = 0L
+
+    private val _state = MutableStateFlow<GameState>(GameState.Loading)
+    val state = _state.asStateFlow()
+
+    init {
+        startGame()
+    }
+
+    fun startGame() {
+        _state.value = GameState.Playing
+    }
+
+    fun setStartTime() {
+        startTime = System.currentTimeMillis()
+    }
+
+    fun setEndTime() {
+        endTime = System.currentTimeMillis()
+        _state.value = GameState.Ended
+    }
+
+    fun loseGame() {
+        _state.value = GameState.Lost
+    }
+}
