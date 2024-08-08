dd88ab10ba0ba93247380c34ff3e555d2d79255a
anddev404 <marcinek07@gmail.com>
2024-08-08 11:00:44 +0200

    Provide class ErrorMessages in AppModule

--- app/src/main/java/com/example/aimtrainer/di/AppModule.kt
+++ app/src/main/java/com/example/aimtrainer/di/AppModule.kt
@@ -1,10 +1,21 @@
 package com.example.aimtrainer.di
 
+import android.app.Application
+import com.example.aimtrainer.auth.domain.use_case.ErrorMessages
+import com.example.aimtrainer.auth.domain.use_case.ErrorMessagesImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideErrorMessages(app: Application): ErrorMessages {
+        return ErrorMessagesImpl(app.resources)
+    }
 }
