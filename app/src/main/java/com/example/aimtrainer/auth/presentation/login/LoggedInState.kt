0518b5932b4dda66e1ace7dcb5f17408cedd9425
anddev404 <marcinek07@gmail.com>
2024-08-10 19:02:01 +0200

    Update LoggedInState.kt

--- app/src/main/java/com/example/aimtrainer/auth/presentation/login/LoggedInState.kt
+++ app/src/main/java/com/example/aimtrainer/auth/presentation/login/LoggedInState.kt
@@ -3,7 +3,7 @@ package com.example.aimtrainer.auth.presentation.login
 import com.example.aimtrainer.auth.domain.model.User
 
 data class LoggedInState(
-    val loadingInProgress: Boolean = false,
+    val loggingInProgress: Boolean = false,
     val user: User? = null,
     val error: String? = null
 )
