1357a499f08e01c55a4d985eaa4c18e3df9c6099
anddev404 <marcinek07@gmail.com>
2024-08-10 19:02:53 +0200

    Update AppModule.kt

--- app/src/main/java/com/example/aimtrainer/di/AppModule.kt
+++ app/src/main/java/com/example/aimtrainer/di/AppModule.kt
@@ -1,8 +1,11 @@
 package com.example.aimtrainer.di
 
 import android.app.Application
-import com.example.aimtrainer.auth.domain.use_case.ErrorMessages
-import com.example.aimtrainer.auth.domain.use_case.ErrorMessagesImpl
+import com.example.aimtrainer.auth.data.remote.FirebaseAuthService
+import com.example.aimtrainer.auth.data.repository.AuthRepositoryImpl
+import com.example.aimtrainer.auth.domain.errors.ErrorMessages
+import com.example.aimtrainer.auth.domain.errors.ErrorMessagesImpl
+import com.example.aimtrainer.auth.domain.repository.AuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,5 +20,17 @@ class AppModule {
     @Singleton
     fun provideErrorMessages(app: Application): ErrorMessages {
         return ErrorMessagesImpl(app.resources)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuthService(): FirebaseAuthService {
+        return FirebaseAuthService()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(firebaseAuthService: FirebaseAuthService): AuthRepository {
+        return AuthRepositoryImpl(firebaseAuthService)
     }
 }
