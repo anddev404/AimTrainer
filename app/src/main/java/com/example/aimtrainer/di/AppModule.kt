c46a1f6121ac754162fb645a8c6b5feecb4986a5
anddev404 <marcinek07@gmail.com>
2024-08-11 10:59:56 +0200

    Add Firebase database providers in Hilt module

--- app/src/main/java/com/example/aimtrainer/di/AppModule.kt
+++ app/src/main/java/com/example/aimtrainer/di/AppModule.kt
@@ -2,10 +2,13 @@ package com.example.aimtrainer.di
 
 import android.app.Application
 import com.example.aimtrainer.auth.data.remote.FirebaseAuthService
+import com.example.aimtrainer.auth.data.remote.FirebaseDatabaseService
 import com.example.aimtrainer.auth.data.repository.AuthRepositoryImpl
+import com.example.aimtrainer.auth.data.repository.DatabaseRepositoryImpl
 import com.example.aimtrainer.auth.domain.errors.ErrorMessages
 import com.example.aimtrainer.auth.domain.errors.ErrorMessagesImpl
 import com.example.aimtrainer.auth.domain.repository.AuthRepository
+import com.example.aimtrainer.auth.domain.repository.DatabaseRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -32,5 +35,17 @@ class AppModule {
     @Singleton
     fun provideAuthRepository(firebaseAuthService: FirebaseAuthService): AuthRepository {
         return AuthRepositoryImpl(firebaseAuthService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseDatabaseService(): FirebaseDatabaseService {
+        return FirebaseDatabaseService()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabaseRepository(firebaseDatabaseService: FirebaseDatabaseService): DatabaseRepository {
+        return DatabaseRepositoryImpl(firebaseDatabaseService)
     }
 }
