a8cbee98c1d9ba8010b85170a92e8e3ddc1169e9
anddev404 <marcinek07@gmail.com>
2024-08-17 08:23:03 +0200

    Update StyledTextField.kt

--- app/src/main/java/com/example/aimtrainer/core/presentation/StyledTextField.kt
+++ app/src/main/java/com/example/aimtrainer/core/presentation/StyledTextField.kt
@@ -1,18 +1,23 @@
 package com.example.aimtrainer.core.presentation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.aimtrainer.ui.theme.Tab_Bottom_Gradient
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.aimtrainer.ui.theme.TextFieldContainerColor
 
 @Composable
 fun StyledTextField(
@@ -29,19 +34,39 @@ fun StyledTextField(
         isError = isError,
         supportingText = supportingText,
         keyboardOptions = keyboardOptions,
-        visualTransformation = visualTransformation
+        visualTransformation = visualTransformation,
+        shape = RoundedCornerShape(16.dp),
+        textStyle = TextStyle(fontSize = 24.sp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = TextFieldContainerColor,
+            unfocusedContainerColor = TextFieldContainerColor,
+            errorContainerColor = TextFieldContainerColor,
+            errorIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedTextColor = Color.White,
+            unfocusedTextColor = Color.White,
+        ),
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun StyledTextFieldPreview() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Tab_Bottom_Gradient),
-        contentAlignment = Alignment.Center
-    ) {
+            .padding(50.dp),
+
+        ) {
+        StyledTextField(
+            value = TextFieldValue("example text"),
+            isError = false
+        )
+        StyledTextField(
+            isError = false
+        )
         StyledTextField(
             isError = true,
             supportingText = { Text(text = "error") })
