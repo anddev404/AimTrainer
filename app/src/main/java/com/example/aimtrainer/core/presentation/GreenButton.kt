aa98482b1d381ce3490f3deff6f1f527cf1d5c7c
anddev404 <marcinek07@gmail.com>
2024-08-18 09:32:22 +0200

    Update GreenButton.kt

--- app/src/main/java/com/example/aimtrainer/core/presentation/GreenButton.kt
+++ app/src/main/java/com/example/aimtrainer/core/presentation/GreenButton.kt
@@ -1,20 +1,16 @@
 package com.example.aimtrainer.core.presentation
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.aimtrainer.R
 import com.example.aimtrainer.ui.theme.DialogBoxTextBorderColor
@@ -22,19 +18,16 @@ import com.example.aimtrainer.ui.theme.DialogBoxTextBorderColor
 @Preview(showBackground = true)
 @Composable
 fun GreenButton(text: String = "Button", onClick: () -> Unit = {}) {
-    Button(
-        onClick = { onClick() },
-        colors = ButtonDefaults.buttonColors(containerColor = Color.Unspecified),
+
+    Box(
+        Modifier.draw9Patch(LocalContext.current, R.drawable.button_green),
+        contentAlignment = Alignment.Center,
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
+
+        Button(
+            onClick = { onClick() },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Unspecified),
         ) {
-            Image(
-                modifier = Modifier.widthIn(200.dp),
-                contentScale = ContentScale.FillWidth,
-                painter = painterResource(id = R.drawable.button_green),
-                contentDescription = "",
-            )
             OutlinedText(
                 text = text,
                 fontSize = 28.sp,
