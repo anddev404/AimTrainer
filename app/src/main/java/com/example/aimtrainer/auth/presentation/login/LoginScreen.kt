0f4d1f2fed396ef141ab8ff1fb720d1bff159da2
anddev404 <marcinek07@gmail.com>
2024-08-19 11:22:18 +0200

    Update LoginScreen.kt

--- app/src/main/java/com/example/aimtrainer/auth/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/aimtrainer/auth/presentation/login/LoginScreen.kt
@@ -2,15 +2,18 @@ package com.example.aimtrainer.auth.presentation.login
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -18,17 +21,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.aimtrainer.R
 import com.example.aimtrainer.auth.domain.model.ValidationResult
+import com.example.aimtrainer.core.presentation.DoubleGradientBox
 import com.example.aimtrainer.core.presentation.GreenButton
+import com.example.aimtrainer.core.presentation.OutlinedText
 import com.example.aimtrainer.core.presentation.StyledTextField
 import com.example.aimtrainer.navigation.Screen
+import com.example.aimtrainer.ui.theme.DialogBoxMainTextColor
+import com.example.aimtrainer.ui.theme.DialogBoxTextColor
+import com.example.aimtrainer.ui.theme.TabBorderColor
 
 @Composable
 fun LoginScreen(
@@ -36,59 +48,117 @@ fun LoginScreen(
     navController: NavController,
     viewModel: LoginViewModel = hiltViewModel(),
 ) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
 
-        val context = LocalContext.current
-        val loginState by viewModel.loginForm.collectAsState()
-        val passwordState by viewModel.passwordForm.collectAsState()
-        val loggedInState by viewModel.loggedIn.collectAsState()
+    val context = LocalContext.current
+    val loginState by viewModel.loginForm.collectAsState()
+    val passwordState by viewModel.passwordForm.collectAsState()
+    val loggedInState by viewModel.loggedIn.collectAsState()
 
-        LaunchedEffect(loggedInState) {
-            loggedInState.user?.let {
-                navController.navigate(Screen.RankScreen)
-            }
-            loggedInState.error?.let {
-                Toast.makeText(context, "${loggedInState.error}", Toast.LENGTH_LONG).show();
-            }
+    LaunchedEffect(loggedInState) {
+        loggedInState.user?.let {
+            navController.navigate(Screen.RankScreen)
         }
+        loggedInState.error?.let {
+            Toast.makeText(context, "${loggedInState.error}", Toast.LENGTH_LONG).show()
+        }
+    }
 
-        if (loggedInState.loggingInProgress) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+    if (loggedInState.loggingInProgress) {
+        Box(contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    } else {
 
-        } else {
-            Spacer(modifier = Modifier.height(24.dp))
-
-            StyledTextField(
-                value = loginState.textFieldValue,
-                onValueChange = { viewModel.updateLoginField(it) },
-                isError = isError(loginState.validationResult),
-                supportingText = { ShowErrorMessage(validationResult = loginState.validationResult) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email
+        DoubleGradientBox(topContent = {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedText(
+                    text = stringResource(id = R.string.login_or_sign_up),
+                    textColor = DialogBoxMainTextColor,
+                    borderColor = TabBorderColor,
+                    fontSize = 32.sp,
+                    fontFamily = FontFamily(Font(R.font.font_default))
                 )
-            )
+            }
+        }, bottomContent = {
+            val scrollState = rememberScrollState()
 
-            Spacer(modifier = Modifier.height(24.dp))
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
 
-            StyledTextField(
-                value = passwordState.textFieldValue,
-                onValueChange = { viewModel.updatePasswordField(it) },
-                isError = isError(passwordState.validationResult),
-                supportingText = { ShowErrorMessage(validationResult = passwordState.validationResult) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password
-                ),
-                visualTransformation = PasswordVisualTransformation()
-            )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(
+                        text = stringResource(id = R.string.enter_email),
+                        color = DialogBoxTextColor,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.font_default))
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    StyledTextField(
+                        value = loginState.textFieldValue,
+                        onValueChange = { viewModel.updateLoginField(it) },
+                        isError = isError(loginState.validationResult),
+                        supportingText = { ShowErrorMessage(validationResult = loginState.validationResult) },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Email
+                        )
+                    )
 
-            Spacer(modifier = Modifier.height(24.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(
+                        text = stringResource(id = R.string.enter_password),
+                        color = DialogBoxTextColor,
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.font_default))
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    StyledTextField(
+                        value = passwordState.textFieldValue,
+                        onValueChange = { viewModel.updatePasswordField(it) },
+                        isError = isError(passwordState.validationResult),
+                        supportingText = { ShowErrorMessage(validationResult = passwordState.validationResult) },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        ),
+                        visualTransformation = PasswordVisualTransformation()
+                    )
 
-            GreenButton(stringResource(id = R.string.sign_in)) { viewModel.validateAndLogin() }
-        }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    GreenButton(stringResource(id = R.string.sign_in)) { viewModel.validateAndLogin() }
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Spacer(modifier = Modifier.height(48.dp))
+                    GreenButton(
+                        stringResource(id = R.string.sign_in_google),
+                        fontSize = 20.sp,
+                        painter = painterResource(id = R.drawable.icon_google)
+                    ) { }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    GreenButton(
+                        stringResource(id = R.string.sign_in_facebook),
+                        fontSize = 20.sp,
+                        painter = painterResource(id = R.drawable.icon_fb)
+                    ) { }
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        })
     }
 }
 
