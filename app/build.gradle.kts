a5a03684f101bdc65e599a049010ace86284692d
anddev404 <marcinek07@gmail.com>
2024-08-27 10:10:21 +0200

    Update SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.aimtrainer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.aimtrainer"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
